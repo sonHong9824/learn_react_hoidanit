6c1576c21c7168ded7a6c65fdedc8c4f481cc475
sonhong9824 <nghongson9824@gmail.com>
2025-06-03 01:58:40 +0700

    31

--- src/components/learn/MyFunction.jsx
+++ src/components/learn/MyFunction.jsx
@@ -1,14 +1,25 @@
 import './style.css'
 
 const MyFunction = () => {
+  const son = "Hong Son"
+  const num = 98
+  const arr = [1,2,3,4,5]
+  const obj = {
+    name: "Son",
+    gender: "male"
+  }
   return (
     <>
-      <div>Nguyen Hng Sn update</div>
+      <div>{son} {num} update</div>
       <div className='date' 
         style={
           {fontSize : "20px"}
         } 
       >09-08-2004</div>
+      <div>{JSON.stringify(arr)}</div>
+      <div>{obj.gender}</div>
+      <div>{JSON.stringify(obj)}</div>
+      <div>{console.log("son nè")}</div>
     </>
     
   )
