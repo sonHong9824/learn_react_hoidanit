96cfa4296d1433019d3af90cb5286683cfd3f149
sonhong9824 <nghongson9824@gmail.com>
2025-06-05 09:41:15 +0700

    36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -3,11 +3,23 @@ const TodoAdd = (props) => {
 
     const {AddNewTodo} = props
     // AddNewTodo("son")
+    const handleClick = () => {
+        alert("click me")
+    }
+
+    const handleChange = (name) => {
+        console.log(">> change", name)
+    }
 
     return(
         <div className='todo-add'>
-            <input type="text"/>
-            <button>Add</button>
+            <input 
+                type="text"
+                onChange={(event) => {handleChange(event.target.value)}}/>
+            <button
+                style={{cursor: "pointer"}}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
